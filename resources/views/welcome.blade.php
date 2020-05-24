b58129aeeea977bb8c5ded78345d7f6fb0a330f1
alirezaporthos <alirezaportous@gmial.com>
2020-05-24 07:30:25 -0700

    added a yes on welcome page just for checking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel oh yes
+                    Laravel oh yes yess
                 </div>
 
                 <div class="links">
